880a63a14cd9a2e2e067e5bf1cc447844ddf2c20
Esteban Mercado <v.estebanmercado@gmail.com>
2018-09-25 10:43:58 -0300

    App Structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import WeatherLocation from './components/WeatherLocation';
+import WeatherLocation from './components/WeatherLocation/';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Weather App (aplicación del clima)!
         <WeatherLocation></WeatherLocation>
       </div>
     );
---
+++ src/components/WeatherLocation/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Location from './Location';
+import WeatherData from './WeatherData';
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY,
+} from './../../constants/weathers';
+import './styles.css';
+
+const data = {
+    temperature: 6,
+    weatherState: SUN,
+    humidity: 10,
+    wind: '10 m/s',
+}
+
+const data2 = {
+    temperature: 15,
+    weatherState: WINDY,
+    humidity: 20,
+    wind: '10 m/s',
+}
+
+class WeatherLocation extends Component {
+    constructor() {
+        super();
+        this.state = {
+            city: "Buenos Aires",
+            data: data,
+        };
+    }
+    handleUpdateClick = () => {
+        console.log("Actualizado");
+
+        this.setState({
+            city: "London",
+            data: data2,
+        });
+    }
+
+    render() {
+        const { city, data } = this.state;
+        return (
+            <div className="weatherLocationCont">
+                <Location city={city}></Location>
+                <WeatherData data={data}></WeatherData>
+                <button onClick={this.handleUpdateClick}>Actualizar</button>
+            </div>
+        );
+    }
+};
+
+export default WeatherLocation;
